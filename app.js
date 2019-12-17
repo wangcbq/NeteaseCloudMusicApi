d9238a5d6805e7269172937aad31d07e9e91e99e
wangcb <wangcb@pukkasoft.cn>
2019-12-17 18:24:53 +0800

    ✨ 用户私密信息加密

--- app.js
+++ app.js
@@ -6,12 +6,26 @@ const request = require('./util/request')
 const packageJSON = require('./package.json')
 const exec = require('child_process').exec
 const cache = require('apicache').middleware
+const os = require('os')
+const CryptoJS = require('crypto-js');
+
+const key = CryptoJS.enc.Utf8.parse('1234123412ABCDEF');//十六位十六进制数作为密钥
+const iv = CryptoJS.enc.Utf8.parse('ABCDEF1234123412');//十六位十六进制数作为密钥偏移量
+
+//解密方法
+function Decrypt(word) {
+  let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
+  let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+  let decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+  let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+  return decryptedStr.toString();
+}
 
 // version check
 exec('npm info NeteaseCloudMusicApi version', (err, stdout, stderr) => {
-  if(!err){
+  if (!err) {
     let version = stdout.trim()
-    if(packageJSON.version < version){
+    if (packageJSON.version < version) {
       console.log(`最新版本: ${version}, 当前版本: ${packageJSON.version}, 请及时更新`)
     }
   }
@@ -21,7 +35,7 @@ const app = express()
 
 // CORS & Preflight request
 app.use((req, res, next) => {
-  if(req.path !== '/' && !req.path.includes('.')){
+  if (req.path !== '/' && !req.path.includes('.')) {
     res.set({
       'Access-Control-Allow-Credentials': true,
       'Access-Control-Allow-Origin': req.headers.origin || '*',
@@ -37,7 +51,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   req.cookies = {}, (req.headers.cookie || '').split(/\s*;\s*/).forEach(pair => {
     let crack = pair.indexOf('=')
-    if(crack < 1 || crack == pair.length - 1) return
+    if (crack < 1 || crack == pair.length - 1) return
     req.cookies[decodeURIComponent(pair.slice(0, crack)).trim()] = decodeURIComponent(pair.slice(crack + 1)).trim()
   })
   next()
@@ -45,7 +59,7 @@ app.use((req, res, next) => {
 
 // body parser
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // cache
 app.use(cache('2 minutes', ((req, res) => res.statusCode === 200)))
@@ -61,12 +75,15 @@ const special = {
 }
 
 fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
-  if(!file.endsWith('.js')) return
+  if (!file.endsWith('.js')) return
   let route = (file in special) ? special[file] : '/' + file.replace(/\.js$/i, '').replace(/_/g, '/')
   let question = require(path.join(__dirname, 'module', file))
 
   app.use(route, (req, res) => {
-    let query = Object.assign({}, req.query, req.body, {cookie: req.cookies})
+    let query = Object.assign({}, req.query, req.body, { cookie: req.cookies })
+    if ('phone' in query) query.phone = Decrypt(query.phone);
+    if ('email' in query) query.email = Decrypt(query.email);
+    if ('password' in query) query.password = Decrypt(query.password);
     question(query, request)
       .then(answer => {
         console.log('[OK]', decodeURIComponent(req.originalUrl))
@@ -75,15 +92,23 @@ fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
       })
       .catch(answer => {
         console.log('[ERR]', decodeURIComponent(req.originalUrl))
-        if(answer.body.code == '301') answer.body.msg = '需要登录'
+        if (answer.body.code == '301') answer.body.msg = '需要登录'
         res.append('Set-Cookie', answer.cookie)
         res.status(answer.status).send(answer.body)
       })
   })
 })
 
+let localhost = ''
+try {
+  const network = os.networkInterfaces()
+  localhost = network[Object.keys(network)[0]][1].address
+} catch (e) {
+  localhost = 'localhost'
+}
+
 const port = process.env.PORT || 3000
-const host = process.env.HOST || ''
+const host = process.env.HOST || localhost
 
 app.server = app.listen(port, host, () => {
   console.log(`server running @ http://${host ? host : 'localhost'}:${port}`)
